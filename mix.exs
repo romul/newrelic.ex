0a94ab754cecbe992a646a4e11c380e913b24947
Roman Smirnov <POMAHC@gmail.com>
2019-08-13 11:46:40 +0300

    Realease a new version

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule NewRelic.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :new_relic,
-     version: "0.1.1",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "Elixir library for sending metrics to New Relic.",
-     package: package(),
-     deps: deps()]
+    [
+      app: :new_relic,
+      version: "0.1.2",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: "Elixir library for sending metrics to New Relic.",
+      package: package(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -19,26 +21,21 @@ defmodule NewRelic.Mixfile do
     [mod: {NewRelic, []}, applications: [:logger, :lhttpc, :poison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:phoenix, "~> 1.2"},
-     {:ecto, ">= 1.1.0"},
-     {:lhttpc, "~> 1.4"},
-     {:poison, "~> 2.2 or ~> 3.0"},
-     {:ex_doc, ">= 0.0.0", only: :dev}]
+    [
+      {:phoenix, "~> 1.2", optional: true},
+      {:ecto, ">= 2.0.0", optional: true},
+      {:lhttpc, "~> 1.4"},
+      {:poison, ">= 2.0.0"},
+      {:ex_doc, "~> 0.19", only: :dev}
+    ]
   end
 
   defp package do
-    [maintainers: ["Roman Smirnov"],
-     licenses: ["MIT"],
-     links: %{"Github" => "https://github.com/romul/newrelic.ex"}]
+    [
+      maintainers: ["Roman Smirnov"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/romul/newrelic.ex"}
+    ]
   end
 end
